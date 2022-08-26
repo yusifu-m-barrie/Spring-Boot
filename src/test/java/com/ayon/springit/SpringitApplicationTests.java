f96c23bca93f840878366a6f76b8ad44346dde79
Yusifu M. Barrie <student3.ubuntuafrika@gmail.com>
2022-08-26 10:53:53 +0100

    adding files

---
+++ src/test/java/com/ayon/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayon.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
